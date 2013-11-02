c63822417e80cf1a75a82d31537eb4b675d6fdff
Rishi Kapadia <rishi.n.kapadia@gmail.com>
2013-11-01 19:32:40 -0700

    differences function

--- Players/player.py
+++ Players/player.py
@@ -1,4 +1,6 @@
 import Game
+STAY_SILENT = Game.STAY_SILENT
+RAT_OUT = Game.RAT_OUT
 
 class Player():
     def __init__(self, _scoreboard, _ID):
@@ -8,14 +10,21 @@ class Player():
         self.myresults = []
 
 
+    def differences(self):
+        num_round = self.scoreboard.get_round_number()
+        if self.scoreboard.get_round_number():
+            res = self.scoreboard.get_result(num_round - 1)
+            if self.myresults[num_round] == STAY_SILENT and (res == 0 or res == 2):
+                self.lawyerdiff = self.lawyerdiff + 1
+            if self.myresults[num_round] == RAT_OUT and (res == 1 or res == 3):
+                self.lawyerdiff = self.lawyerdiff + 1    
+
+
+
     def get_move(self):
         return Game.RAT_OUT
         #for i in range(self.scoreboard.get_round_number()):
-        num_round = self.scoreboard.get_round_number()
-        if self.scoreboard.get_round_number():
-            res = self.scoreboard.get_result(num_round)
-            #if self.myresults != res:
-                
+        differences()
         from random import random
         return int(random() + 1)
         #return Game.STAY_SILENT
